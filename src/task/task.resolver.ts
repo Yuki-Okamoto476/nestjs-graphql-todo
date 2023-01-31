1f934859a8bd47ab87812e2430a349e3f3e77f03
OKAMOTO YUKI <okayu110108@gmail.com>
2023-01-31 22:05:58 +0900

    タスク取得、作成機能の実装

---
+++ src/task/task.resolver.ts
@@ -0,0 +1,22 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Task } from './models/task.model';
+import { TaskService } from './task.service';
+
+@Resolver()
+export class TaskResolver {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Query(() => [Task], { nullable: 'items' })
+  getTasks(): Task[] {
+    return this.taskService.getTasks();
+  }
+
+  @Mutation(() => Task)
+  createTask(
+    @Args('name') name: string,
+    @Args('dueDate') dueDate: string,
+    @Args('description', { nullable: true }) description: string,
+  ): Task {
+    return this.taskService.createTask(name, dueDate, description);
+  }
+}
