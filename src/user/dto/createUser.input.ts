6cc197cf957fb8754527b3b02a8a73ef4ad11dc2
OKAMOTO YUKI <okayu110108@gmail.com>
2023-03-07 21:33:12 +0900

    Implement create user

---
+++ src/user/dto/createUser.input.ts
@@ -0,0 +1,16 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, MinLength } from 'class-validator';
+
+@InputType()
+export class CreateUserInput {
+  @Field()
+  name: string;
+
+  @Field()
+  @IsEmail()
+  email: string;
+
+  @Field()
+  @MinLength(8)
+  password: string;
+}
