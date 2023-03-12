5f7a8554ef5f85f53f46d429296bb21582b93e6c
OKAMOTO YUKI <okayu110108@gmail.com>
2023-03-12 22:23:19 +0900

    Migrate db

--- backend/prisma/migrations/20230312131928_init/migration.sql
+++ backend/prisma/migrations/20230312131928_init/migration.sql
@@ -1,11 +1,19 @@
-/*
-  Warnings:
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('NOT_STARTED', 'IN_PROGRESS', 'COMPLETED');
 
-  - Added the required column `userId` to the `Task` table without a default value. This is not possible if the table is not empty.
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "dueDate" VARCHAR(10) NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'NOT_STARTED',
+    "description" TEXT,
+    "createdAt" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(0) NOT NULL,
+    "userId" INTEGER NOT NULL,
 
-*/
--- AlterTable
-ALTER TABLE "Task" ADD COLUMN     "userId" INTEGER NOT NULL;
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "User" (
