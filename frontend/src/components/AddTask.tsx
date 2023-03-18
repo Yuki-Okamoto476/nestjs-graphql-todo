18409ab91720fa1c86f7352b31cbd6ab7f7584ce
OKAMOTO YUKI <okayu110108@gmail.com>
2023-03-18 22:24:34 +0900

    Introduce react-hook-form into AddTask

--- frontend/src/components/AddTask.tsx
+++ frontend/src/components/AddTask.tsx
@@ -10,6 +10,7 @@ import { Task } from '../types/task';
 import { CREATE_TASK } from '../queries/taskMutations';
 import { GET_TASKS } from '../queries/taskQueries';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 type AddTaskProps = {
   userId: number;
@@ -17,55 +18,42 @@ type AddTaskProps = {
 
 export const AddTask: FC<AddTaskProps> = ({ userId }) => {
   const [open, setOpen] = useState(false);
-  const [name, setName] = useState('');
-  const [dueDate, setDueDate] = useState('');
-  const [description, setDescription] = useState('');
-  const [isInvalidName, setIsInValidName] = useState(false);
-  const [isInvalidDueDate, setIsInvalidDueDate] = useState(false);
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: '',
+      dueDate: '',
+      description: '',
+    },
+  });
 
   const [createTask] = useMutation<{ createTask: Task }>(CREATE_TASK);
-
-  const resetState = () => {
-    setName('');
-    setDueDate('');
-    setDescription('');
-    setIsInValidName(false);
-    setIsInvalidDueDate(false);
-  };
-
-  const handleAddTask = async () => {
-    let canAdd = true;
-    if (!name) {
-      canAdd = false;
-      setIsInValidName(true);
-    } else {
-      setIsInValidName(false);
-    }
-    if (!Date.parse(dueDate)) {
-      canAdd = false;
-      setIsInvalidDueDate(true);
-    } else {
-      setIsInvalidDueDate(false);
-    }
-    if (canAdd) {
-      const createTaskInput = { name, dueDate, description, userId };
-      try {
-        await createTask({
-          variables: { createTaskInput },
-          refetchQueries: [{ query: GET_TASKS, variables: { userId } }],
-        });
-        resetState();
-        setOpen(false);
-      } catch (error: any) {
-        if (error.message === 'Unauthorized') {
-          localStorage.getItem('token');
-          alert('トークンの有効期限が切れました。サインイン画面に遷移します。');
-          navigate('/signin');
-          return;
-        }
-        alert('タスクの登録に失敗しました');
+  const handleAddTask = async (data: {
+    name: string;
+    dueDate: string;
+    description?: string;
+  }) => {
+    const createTaskInput = { ...data, userId };
+    try {
+      await createTask({
+        variables: { createTaskInput },
+        refetchQueries: [{ query: GET_TASKS, variables: { userId } }],
+      });
+      reset();
+      setOpen(false);
+    } catch (error: any) {
+      if (error.message === 'Unauthorized') {
+        localStorage.getItem('token');
+        alert('トークンの有効期限が切れました。サインイン画面に遷移します。');
+        navigate('/signin');
+        return;
       }
+      alert('タスクの登録に失敗しました');
     }
   };
 
@@ -74,7 +62,7 @@ export const AddTask: FC<AddTaskProps> = ({ userId }) => {
   };
 
   const handleClose = () => {
-    resetState();
+    reset();
     setOpen(false);
   };
 
@@ -88,49 +76,54 @@ export const AddTask: FC<AddTaskProps> = ({ userId }) => {
         Add Task
       </Button>
       <Dialog fullWidth={true} maxWidth='sm' open={open} onClose={handleClose}>
-        <DialogTitle>Add Task</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin='normal'
-            id='name'
-            label='Task Name'
-            fullWidth
-            required
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            error={isInvalidName}
-            helperText={isInvalidName && 'タスク名を入力してください'}
-          />
-          <TextField
-            autoFocus
-            margin='normal'
-            id='due-date'
-            label='Due Date'
-            placeholder='yyyy-mm-dd'
-            fullWidth
-            required
-            value={dueDate}
-            onChange={(e) => setDueDate(e.target.value)}
-            error={isInvalidDueDate}
-            helperText={isInvalidDueDate && '日付形式で入力してください'}
-          />
-          <TextField
-            autoFocus
-            margin='normal'
-            id='description'
-            label='Description'
-            fullWidth
-            multiline
-            rows={4}
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleAddTask}>Add</Button>
-        </DialogActions>
+        <form onSubmit={handleSubmit(handleAddTask)}>
+          <DialogTitle>Add Task</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin='normal'
+              id='name'
+              label='タイトル'
+              fullWidth
+              error={!!errors.name}
+              helperText={errors.name?.message}
+              {...register('name', {
+                required: { value: true, message: '名前は必須項目です' },
+              })}
+            />
+            <TextField
+              autoFocus
+              margin='normal'
+              id='dueDate'
+              label='期日'
+              placeholder='yyyy-mm-dd'
+              fullWidth
+              error={!!errors.dueDate}
+              helperText={errors.dueDate?.message}
+              {...register('dueDate', {
+                required: { value: true, message: '期日は必須項目です' },
+                pattern: {
+                  value: /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/,
+                  message: 'yyyy-mm-dd形式で入力してください',
+                },
+              })}
+            />
+            <TextField
+              autoFocus
+              margin='normal'
+              id='description'
+              label='説明'
+              fullWidth
+              multiline
+              rows={4}
+              {...register('description')}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button type='submit'>Add</Button>
+          </DialogActions>
+        </form>
       </Dialog>
     </div>
   );
